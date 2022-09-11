8a54bc5decaadb7d7a7af6af71339d61d0305024
Jacob Paris <jacob@jacobparis.com>
2022-09-11 23:06:48 +0000

    fix: set target to netlify

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,13 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: [".*"],
+  serverBuildTarget: "netlify",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
